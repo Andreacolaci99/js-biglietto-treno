8ae2c1ad1eea25c883fd81c59add0becc5ac9462
Andreacolaci99 <colaciandrea99@gmail.com>
2024-12-13 18:36:13 +0100

    feat : add price

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,21 @@
-let variable1 = "ciao";
 
-console.log(variable1);
+let distance = prompt ("Quanti chilometri vuoi percorrere ?");
+let age = prompt("Inserisci la tua età");
+let km_price = 0.21 ;
+let finalprice = distance * km_price;
+
+if(!isNaN(distance) && Number(age)<18){
+ 
+    
+       let sconto = finalprice * 0.20;
+
+        alert(`Hai diritto a uno sconto del 20%! Il prezzo finale è €${finalprice.toFixed(2)}`);
+
+    }else if (!isNaN(distance) && Number(age)>65){
+
+        let sconto = finalprice * 0.40;
+
+        alert(`Hai diritto a uno sconto del 40%! Il prezzo finale è €${finalprice.toFixed(2)}`);
+    }else{
+        console.log(finalprice)
+}
