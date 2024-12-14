ae988b3bd8555b70577a72a162b61a7ccb9e7271
Andreacolaci99 <colaciandrea99@gmail.com>
2024-12-14 10:58:20 +0100

    feat : End Exercise

--- JS/main.js
+++ JS/main.js
@@ -3,19 +3,21 @@ let distance = prompt ("Quanti chilometri vuoi percorrere ?");
 let age = prompt("Inserisci la tua età");
 let km_price = 0.21 ;
 let finalprice = distance * km_price;
+const scontogiovani = finalprice - (finalprice * 0.20);
+const scontoanziani = finalprice - (finalprice * 0.40);
 
-if(!isNaN(distance) && Number(age)<18){
+if (!isNaN(distance)){
+
+     if (Number(age)<18){
  
-    
-       let sconto = finalprice * 0.20;
-
-        alert(`Hai diritto a uno sconto del 20%! Il prezzo finale è €${finalprice.toFixed(2)}`);
+    alert(`Hai diritto a uno sconto del 20%! Il prezzo finale è € ${scontogiovani.toFixed(2)}`);
 
     }else if (!isNaN(distance) && Number(age)>65){
 
-        let sconto = finalprice * 0.40;
+    alert(`Hai diritto a uno sconto del 40%! Il prezzo finale è € ${scontoanziani.toFixed(2)}`);
 
-        alert(`Hai diritto a uno sconto del 40%! Il prezzo finale è €${finalprice.toFixed(2)}`);
-    }else{
-        console.log(finalprice)
+    }else {
+        alert(`Pagherai tariffa intera di € ${finalprice.toFixed(2)}`);
+    }
 }
+ 
